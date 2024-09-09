45171378c40c301fc3f936021dfacc452c8236bc
quocthoaitran <quocthoaitran@gmail.com>
2024-09-09 17:26:03 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Button type="primary">Hello World</Button>
+    </div>
+  );
+}
